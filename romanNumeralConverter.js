e2511b283ef7b415a6d93baafceda0a212a16648
Dung Vo <dungvo0111@gmail.com>
2020-01-05 15:19:46 +0200

    Roman Converter updated

---
+++ romanNumeralConverter.js
@@ -0,0 +1,32 @@
+function convertToRoman(num) {
+    var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romanNumeral = [
+       "M",
+       "CM",
+       "D",
+       "CD",
+       "C",
+       "XC",
+       "L",
+       "XL",
+       "X",
+       "IX",
+       "V",
+       "IV",
+       "I"
+     ];
+   
+     var result = "";
+   
+     for(var i = 0; i<=decimalValue.length; i++) {
+         while(decimalValue[i] <= num) {
+             result += romanNumeral[i];
+             num -= decimalValue[i];
+         }
+     }
+   
+    return result;
+}
+   
+console.log(convertToRoman(36));
+   
